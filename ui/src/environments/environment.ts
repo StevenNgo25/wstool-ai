c40e3fde4d349742a2ae6856b33d79d6090c823d
Ngô Công <daongocong@vnpt.com>
2025-07-15 13:09:39 +0700

    Update environment.ts

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: "https://localhost:32770/api",
-  imageUrl: "https://localhost:32770"
+  apiUrl: "http://watchsearch-post.com:5000/api",
+  imageUrl: "http://watchsearch-post.com:5000"
+  /* apiUrl: "https://localhost:32770/api",
+  imageUrl: "https://localhost:32770" */
 }
