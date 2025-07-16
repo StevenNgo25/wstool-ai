187bb2b07fe0eeaa8a33ee9eecb872332a21d900
Công Ngô <steven.work.vn@gmail.com>
2025-07-16 22:03:37 +0700

    Update environment.ts

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  /* apiUrl: "http://watchsearch-post.com:5000/api",
-  imageUrl: "http://watchsearch-post.com:5000" */
-  apiUrl: "https://localhost:56676/api",
-  imageUrl: "https://localhost:56676"
+  apiUrl: "http://watchsearch-post.com:5000/api",
+  imageUrl: "http://watchsearch-post.com:5000"
+  /* apiUrl: "https://localhost:32770/api",
+  imageUrl: "https://localhost:32770" */
 }
